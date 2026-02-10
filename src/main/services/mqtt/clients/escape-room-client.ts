5af26f983e22df8d21789ab28ec933355df24291
Zackery Cassar <zackery.cassar@gmail.com>
2026-02-10 13:10:46 +1000

    Fixed an issue where non JSON messages would cause errors to appear.

--- src/main/services/mqtt/clients/escape-room-client.ts
+++ src/main/services/mqtt/clients/escape-room-client.ts
@@ -13,68 +13,77 @@ export class EscapeRoomClient extends BaseClient {
   }
 
   protected callback(topic: string, message: string): void {
-    const data = JSON.parse(message)
+    try {
+      // Try to parse the message as JSON. If it fails, log an error and silently fail.
+      const data = JSON.parse(message)
 
-    // Timer stuff
-    if (topic === `${this.config.topic}/timer/state`) {
-      this.webContents.send('timer:state', {
-        roomId: this.room.id,
-        state: data.state
-      })
-      return
-    }
-    if (topic === `${this.config.topic}/timer/connected`) {
-      this.webContents.send('timer:connected', {
-        roomId: this.room.id,
-        connected: data.connected
-      })
-      return
-    }
-    if (topic === `${this.config.topic}/timer/data`) {
-      this.webContents.send('timer:data', {
-        roomId: this.room.id,
-        data: data.value
-      })
-      return
-    }
-
-    // Hint stuff
-    if (topic === `${this.config.topic}/hint/data`) {
-      this.webContents.send('hint:data', {
-        roomId: this.room.id,
-        data: data.value
-      })
-      return
-    }
-    if (topic === `${this.config.topic}/hint/counter`) {
-      this.webContents.send('hint:counter', {
-        roomId: this.room.id,
-        counter: data.value
-      })
-      return
-    }
-
-    // Loop through every puzzle and see if the topic matches
-    Object.values(this.room.puzzles).forEach((puzzle) => {
-      // Puzzle connected
-      if (topic === `${this.room.mqtt.topic}/${puzzle.subtopic}/connected`) {
-        this.webContents.send('puzzle:connected', {
+      // Timer stuff
+      if (topic === `${this.config.topic}/timer/state`) {
+        this.webContents.send('timer:state', {
           roomId: this.room.id,
-          puzzleId: puzzle.id,
-          connected: data.connected
-        })
-        return
-      }
-      // Puzzle state
-      if (topic === `${this.room.mqtt.topic}/${puzzle.subtopic}/state`) {
-        // Send the puzzle state update to the renderer process
-        this.webContents.send('puzzle:state', {
-          roomId: this.room.id,
-          puzzleId: puzzle.id,
           state: data.state
         })
         return
       }
-    })
+      if (topic === `${this.config.topic}/timer/connected`) {
+        this.webContents.send('timer:connected', {
+          roomId: this.room.id,
+          connected: data.connected
+        })
+        return
+      }
+      if (topic === `${this.config.topic}/timer/data`) {
+        this.webContents.send('timer:data', {
+          roomId: this.room.id,
+          data: data.value
+        })
+        return
+      }
+
+      // Hint stuff
+      if (topic === `${this.config.topic}/hint/data`) {
+        this.webContents.send('hint:data', {
+          roomId: this.room.id,
+          data: data.value
+        })
+        return
+      }
+      if (topic === `${this.config.topic}/hint/counter`) {
+        this.webContents.send('hint:counter', {
+          roomId: this.room.id,
+          counter: data.value
+        })
+        return
+      }
+
+      // Loop through every puzzle and see if the topic matches
+      Object.values(this.room.puzzles).forEach((puzzle) => {
+        // Puzzle connected
+        if (topic === `${this.room.mqtt.topic}/${puzzle.subtopic}/connected`) {
+          this.webContents.send('puzzle:connected', {
+            roomId: this.room.id,
+            puzzleId: puzzle.id,
+            connected: data.connected
+          })
+          return
+        }
+        // Puzzle state
+        if (topic === `${this.room.mqtt.topic}/${puzzle.subtopic}/state`) {
+          // Send the puzzle state update to the renderer process
+          this.webContents.send('puzzle:state', {
+            roomId: this.room.id,
+            puzzleId: puzzle.id,
+            state: data.state
+          })
+          return
+        }
+      })
+    } catch (error) {
+      console.error(
+        `Failed to parse MQTT message for topic ${topic} - It's probably not in a JSON format:`,
+        error
+      )
+      return
+    }
   }
 }
